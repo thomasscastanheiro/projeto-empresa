6de9a2d9641ae86c3f1998c3a675c1f6e7cdc3a3
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2026-06-24 10:13:43 -0300

    Update conexao.php

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+define('SERVIDOR','localhost');
+define('USUARIO','');
+define('SENHA','');
+define('BASEDADOS','empresa26');
+
+$conexao = mysqli_connect(SERVIDOR,USUARIO,SENHA,BASEDADOS);
+
+if(!$conexao) {
+    die("Erro ao conectar com o banco: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conexao,"utf8");
+?>
